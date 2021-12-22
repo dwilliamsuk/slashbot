39490abe2962e016c275d6e8ab8dabb6a6df1373
TheJaffaMeme <contact@jaffasite.ga>
2021-12-22 14:38:32 +0000

    Cleaning up commands

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,6 +1,5 @@
 # __init__.py
 ## This file imports all the commands I'd like to be available for use. If a command isn't in here it means I've disabled it for slashbot for whatever reason.
-#from .coverage import coverage
 from .random import random
 from .image import image
 from .bonk import bonk
@@ -8,11 +7,9 @@ from .weather import weather
 from .info import info
 from .invite import invite
 from .avatar import avatar
-#from .zdebug import zdebug
 from .qrcode import qrcode
 from .cat import cat
 from .dog import dog
-from .shibe import shibe
 from .fox import fox
 from .w3w import w3w
 from .ping import ping
